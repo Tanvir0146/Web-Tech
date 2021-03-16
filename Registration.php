500ec7cac2ad12811695b4e0ae59611896315666
Tanvir0146 <57800090+Tanvir0146@users.noreply.github.com>
2021-03-16 20:06:09 +0600

    Lab Task 1

--- Registration.php
+++ Registration.php
@@ -8,9 +8,6 @@
 <p> 1. Desiging a login form  </p>
 
 <h1> Registration </h1>
-
-<br><br>
-
 <form>
 <table>
 <tr>
@@ -18,53 +15,47 @@
 <td><input type="text" id="fname" name="fname"></td>
 </tr>
 
-<tr>
-<td> Name   : </td>
-<td><input type="text" id="fname" name="fname"></td>
-</tr>
 
 <tr>
 <td> Email  : </td>
-<td><input type="text" id="fname" name="fname"></td>
+<td><input type="text" id="email" name="email"></td>
 </tr>
 
 <tr>
 <td> User Name   : </td>
-<td><input type="text" id="fname" name="fname"></td>
+<td><input type="text" id="uname" name="uname"></td>
 </tr>
 
 <tr>
 <td> Password   : </td>
-<td><input type="text" id="fname" name="fname"></td>
+<td><input type="text" id="password" name="password"></td>
 </tr>
 
 <tr>
 <td> Confirm Password  : </td>
-<td><input type="text" id="fname" name="fname"></td>
+<td><input type="text" id="cpassword" name="cpassword"></td>
+</tr>
+
+<br><br>
+<tr>
+<td> Gender :  </td>
 </tr>
 <tr>
-<td><label1 for="Birthday">Birthday:<label1></td>
+<td><input type="radio" id="Gender" name="Gender" value="Male">Male
+<input type="radio" id="Gender" name="Gender" value="FeMale">Female
+<input type="radio" id="Gender" name="Gender" value="Other">Other</td> 
+
+</tr>
+
+<br><br>
+<tr>
+<td><label1 for="Birthday"> Date of Birth:<label1></td>
 <td><input type="date" id="Birthday" name="Birthday"></td>
 </tr>
 
 </table>
-<br><br>
-<p> Gender  </p>
-<tr>
-<td> Male </td>
-<input type="radio" id="Gender" name="Gender" value="Male"> 
-</tr>
 
-<tr>
-<td> Female</td>
-<input type="radio" id="Gender" name="Gender" value="Male"> 
-</tr>
-
-<tr>
-<td> Other </td>
-<input type="radio" id="Gender" name="Gender" value="Male"> 
-</tr>
-<br><br>
+<br>
 <tr>
 <input type="Submit" Value= "Submit">
 <input type="Reset" Value= "Reset">
